d0e579139668ec4ab6c57e8e89991013cde6d857
Eduardo Ramirez <edramir18@gmail.com>
2018-01-25 16:29:14 -0400

    Bingo serie added

--- public/js/index.js
+++ public/js/index.js
@@ -2,40 +2,65 @@
 
 document.addEventListener('DOMContentLoaded', (event) => {
   console.log('DOM Loaded')
+  updateBingoSerie()
+  document.getElementById('generate').addEventListener('click', e => updateBingoSerie())
+  document.getElementById('start').addEventListener('click', e => startBingo())
+})
 
-  const bingoSerie = [
-    '122122122',
-    '313113111',
-    '111331212',
-    '121212132',
-    '212111223',
-    '131111331'
-  ]
+function startBingo () {
+  const balls = []
+  for (let i = 1; i <= 90; i++) {
+    balls.push(i)
+  }
+  const id = setInterval(nextBall, 400)
+  function nextBall () {
+    if (balls.length === 0) clearInterval(id)
+    const ball = balls.splice(random(balls.length), 1)[0]
+    const cell = document.querySelector(`.ticket__number[data-key="${ball}"]`)
+    if (!cell) return
+    cell.classList.add('-is-marked')
+    if (checkFullTicket()) {
+      console.log('WIN')
+      clearInterval(id)
+    } 
+  }
+
+  function checkFullTicket () {
+    const tickets = document.querySelectorAll('.ticket')
+    for (let e = 0; e < tickets.length; e++) {
+      if (tickets[e].querySelectorAll('.-is-marked').length === 15) return true
+    }
+    return false
+  }
+}
+
+function updateBingoSerie () {
+  const bingoSerie = new Serie()
+  generateSerie(0, bingoSerie)
   const serie = new BingoSerie()
   const ticketElements = document.querySelectorAll('.ticket')
   for (let e = 0; e < ticketElements.length; e++) {
-    const ticket = new BingoTicket(bingoSerie[e])
+    const ticket = new BingoTicket(bingoSerie.data[e])
     ticket.data.forEach((row, i) => {
       row.forEach((item, j) => {
         const index = i * 9 + j
+        const newCell = document.createElement('div')
+        newCell.classList.add('ticket__number')
         if (item === 0) {
-          ticketElements[e].children[index].classList.add('-is-empty')
+          newCell.classList.add('-is-empty')          
         } else {
-          ticketElements[e].children[index].textContent = serie.selectNext(j)
-          ticketElements[e].children[index].addEventListener('click', e => {
-            e.target.classList.toggle('-is-marked')
-          })
+          newCell.textContent = serie.selectNext(j)
+          newCell.setAttribute('data-key', newCell.textContent)
+        }
+        if (index < ticketElements[e].children.length) {
+          ticketElements[e].replaceChild(newCell, ticketElements[e].children[index])
+        } else {
+          ticketElements[e].appendChild(newCell)
         }
       })
     })
   }
-  document.querySelector('button').addEventListener('click', e => {
-    const ticketElements = document.querySelectorAll('.ticket')
-    for (let e = 0; e < ticketElements.length; e++) {
-      console.log(27 - ticketElements[e].querySelectorAll('.-is-empty').length)
-    }
-  })
-})
+}
 
 function random (maxValue) {
   return Math.floor(Math.random() * maxValue)
@@ -76,7 +101,7 @@ function BingoTicket (ticket) {
   }
 
   function init (obj) {
-    const arr = ticket.split('').map(k => Number(k))
+    const arr = ticket.slice(0)
     const rows = {
       one: 0,
       two: 0,
@@ -123,4 +148,101 @@ function BingoTicket (ticket) {
   }
 
   init(this)
+}
+
+function Serie () {
+  this.data = []
+  function init (obj) {
+    for (let i = 0; i < 6; i++) {
+      obj.data.push(Array(9).fill(0))
+    }
+  }
+  this.fillColumn = function (col, arr) {
+    arr.forEach((v, i) => { this.data[i][col] = Number(v) })
+  }
+  this.cleanColumn = function (col) {
+    for (let i = 0; i < 6; i++) {
+      this.data[i][col] = 0
+    }
+  }
+  this.isValid = function (index) {
+    return !this.data.some((row, ri) => {
+      let sum = row.reduce((total, v) => {
+        return total + v
+      }, 0)
+      if ((sum + 8 - index) > 15) return true
+      return false
+    })
+  }
+  this.toString = function () {
+    return this.data.reduce((arr, v) => {
+      arr.push(v.join(''))
+      return arr
+    }, [])
+  }
+
+  init(this)
+}
+
+function permute (str) {
+  const arrPermutes = []
+  const obj = str.split('').reduce((o, v) => {
+    if (o.hasOwnProperty(v)) {
+      o[v]++
+    } else {
+      o[v] = 1
+    }
+    return o
+  }, {})
+  function permutes (result, elements) {
+    if (Object.keys(elements).length === 1 && Object.values(elements)[0] === 1) {
+      arrPermutes.push(result + Object.keys(elements)[0])
+    } else {
+      const local = Object.assign({}, elements)
+      const arr = Object.keys(elements)
+      arr.forEach(k => {
+        const v = local[k]
+        if (v === 1) {
+          delete local[k]
+        } else {
+          local[k]--
+        }
+        permutes(result + k, local)
+        local[k] = v
+      })
+    }
+  }
+  permutes('', obj)
+  return arrPermutes
+}
+
+const first = permute('222111').concat(permute('321111'))
+const middle = permute('331111').concat(permute('322111')).concat(permute('222211'))
+const last = permute('332111').concat(permute('322211')).concat(permute('222221'))
+
+function generateSerie (index, serie) {
+  let local = []
+  switch (index) {
+    case 0:
+      local = first.slice(0)
+      break
+    case 8:
+      local = last.slice(0)
+      break
+    default:
+      local = middle.slice(0)
+  }
+
+  let result = false
+  while (!result && local.length > 0) {
+    const test = local.splice(random(local.length), 1)[0]
+    serie.fillColumn(index, test.split(''))
+    if (serie.isValid(index)) {
+      if (index === 8) return true
+      result = generateSerie(index + 1, serie)
+    } else {
+      serie.cleanColumn(index)
+    }
+  }
+  return result
 }
